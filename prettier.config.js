336047472fd2201776cab40a4bee83c767fba551
WonhyeongLee <bigfile57@naver.com>
2023-08-06 23:06:20 +0900

    init

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  bracketSpacing: true,
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: 'none',
+  endOfLine: 'auto'
+};
