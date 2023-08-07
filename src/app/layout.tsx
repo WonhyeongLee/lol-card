1433d75aa76e0ccf81a29b38369c080f4813eefb
WonhyeongLee <bigfile57@naver.com>
2023-08-07 23:33:21 +0900

    rename: src폴더 아래에 위치하도록 폴더구조 수정
    app은 라우팅관련 컴포넌트들, containers은 독립적인 컴포넌트들(card등 기능 이름의 폴더 밑에 작성), components는 재사용 가능한 컴포넌트들, styles는 전역스타일이나 변수 등 필요한 스타일 파일을 위치하고 나머지도 상황에 맞게 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
 
-import './globals.css';
+import '../styles/globals.css';
 import type { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body className={inter.className}>{children}</body>
     </html>
   );
