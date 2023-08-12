ec4b00a0b8b7c7db976f79e5519e00e7df932b87
WonhyeongLee <bigfile57@naver.com>
2023-08-12 23:59:24 +0900

    feat: cardTitle 컴포넌트의 card 부분 슬라이드로 구현

--- src/containers/card/card-title/CardTitle.tsx
+++ src/containers/card/card-title/CardTitle.tsx
@@ -1,7 +1,8 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { useQuery } from '@apollo/client';
+import useEmblaCarousel from 'embla-carousel-react';
 
 import { GET_CARD_DATA } from '~/src/graphql/queries/cardQuery';
 
@@ -19,6 +20,34 @@ const LolCard = () => {
     GET_CARD_DATA
   );
 
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    loop: true
+  });
+
+  useEffect(() => {
+    if (emblaApi) {
+      // 슬라이드가 변경될 때마다 호출되는 콜백을 설정합니다.
+      emblaApi.on('select', () => {
+        console.log(`Current slide index: ${emblaApi.selectedScrollSnap()}`);
+      });
+      emblaApi.reInit();
+    }
+  }, [emblaApi, data]);
+
+  const scrollPrev = () => {
+    console.log('prev');
+
+    if (emblaApi) {
+      emblaApi.scrollPrev();
+    }
+  };
+
+  const scrollNext = () => {
+    if (emblaApi) {
+      emblaApi.scrollNext();
+    }
+  };
+
   if (loading) {
     return <p>Loading...</p>;
   }
@@ -26,19 +55,29 @@ const LolCard = () => {
     return <p>Error: {error.message}</p>;
   }
 
+  if (!data?.summoner || data.summoner.length === 0) {
+    return <p>작성된 롤카드가 없습니다.</p>;
+  }
+
   return (
-    <article className="text-center w-full">
-      <p className="text-xl font-semibold mb-2">최근 작성한 롤카드 목록</p>
-      <div className="bg-white p-4 rounded-lg shadow w-1/2 overflow-x-auto whitespace-nowrap m-auto">
-        {data?.summoner.map((summoner, index) => (
-          <div key={index} className="mb-4">
-            <p>소환사명: {summoner.information.summonerName}</p>
-            <p>소환사 레벨: {summoner.information.summonerLevel}</p>
-            <p>소환사 아이콘: {summoner.information.summonerIcon}</p>
-          </div>
-        ))}
+    <div className="max-w-screen-lg mx-[220px]">
+      <div className="embla p-4 overflow-hidden" ref={emblaRef}>
+        <div className="embla__container flex ml-[calc(var(--slide-spacing)*-1)]">
+          {data?.summoner?.map((summoner, index) => (
+            <div
+              className="embla__slide flex-none pl-[var(--slide-spacing)] relative w-full"
+              key={index}
+            >
+              <p>소환사명: {summoner.information.summonerName}</p>
+              <p>소환사 레벨: {summoner.information.summonerLevel}</p>
+              <p>소환사 아이콘: {summoner.information.summonerIcon}</p>
+            </div>
+          ))}
+        </div>
       </div>
-    </article>
+      <button onClick={scrollPrev}>Prev</button>
+      <button onClick={scrollNext}>Next</button>
+    </div>
   );
 };
 
