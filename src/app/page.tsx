210079255f2221c8d2f15530c005975a36b22c9d
WonhyeongLee <bigfile57@naver.com>
2023-08-14 20:39:08 +0900

    fix: page의 input부분의 너비가 placeholder 보다 적게 줄어들지 않도록 최소 너비 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <section className="bg-[#f1f3f5] max-w-screen-lg w-full mx-[220px] p-8 flex flex-col items-center">
         <p className="text-4xl font-extrabold mb-4 text-center">LOL-CARD</p>
         <input
-          className="border p-2 rounded-lg w-1/2 mb-4"
+          className="border p-2 rounded-lg w-1/2 min-w-[200px] mb-4 text-center"
           placeholder="소환사명을 입력해주세요"
         />
         <LolCard />
