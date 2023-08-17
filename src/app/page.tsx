e8b3f541f9bbe0c9149b03abe2e807084d583ce2
WonhyeongLee <bigfile57@naver.com>
2023-08-17 22:03:08 +0900

    feat: main 페이지 소환사명 검색 부분에 debounce 적용, 키워드가 정상적으로 호출되는지 확인

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
 'use client';
+import { useState, useCallback, useEffect } from 'react';
+
+import _ from 'lodash';
+
 import LolCard from '../containers/card/card-title/CardTitle';
 
 export default function Home() {
+  const [summonerName, setSummonerName] = useState<string>('');
+
+  const debouncedSearch = useCallback(
+    _.debounce((query: string) => {
+      console.log(`소환자검색 :  ${query}`);
+    }, 1000),
+    []
+  );
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSummonerName(value);
+
+    if (value.trim() === '') {
+      debouncedSearch.cancel();
+    } else {
+      debouncedSearch(value);
+    }
+  };
+
+  useEffect(() => {
+    console.log(summonerName);
+  }, [summonerName]);
+
   return (
     <main className="flex min-h-screen justify-center">
       <section className="mx-[220px] flex w-full max-w-screen-lg flex-col items-center justify-center p-8">
@@ -9,6 +37,8 @@ export default function Home() {
         <input
           className="mb-4 w-1/2 min-w-[200px] rounded-lg border p-2 text-center placeholder-opacity-0 focus:placeholder-opacity-0"
           placeholder="소환사명을 입력해주세요"
+          value={summonerName}
+          onChange={handleInputChange}
         />
         <span>최근 작성한 롤카드</span>
         <LolCard />
