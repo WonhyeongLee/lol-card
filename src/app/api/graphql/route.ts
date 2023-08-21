b59d212027a3d5cf7c16d30514e91a623efd2c79
WonhyeongLee <bigfile57@naver.com>
2023-08-21 20:28:14 +0900

    fix: 기존 api/graphql 요청처리부분을  startServerAndCreateNextHandler 를 사용하도록 변경
    - 엔드포인트안에 직접 POST 요청을 처리하도록 구현했는데 as-intergrations/next 라이브러리를 사용하면 정말 간단하게 구현이 가능해져서 변경했습니다.

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,29 +1,82 @@
-import { mockServer } from '@graphql-tools/mock';
+import { ApolloServer } from '@apollo/server';
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+import { addMocksToSchema } from '@graphql-tools/mock';
 import { makeExecutableSchema } from '@graphql-tools/schema';
-import { print } from 'graphql';
+// import { print } from 'graphql';
 // import { NextApiRequest, NextApiResponse } from 'next';
-import { NextResponse } from 'next/server';
+// import { NextApiRequest } from 'next';
+// import { NextResponse } from 'next/server';
 
 import { typeDefs } from '../../../graphql/schema';
-import { mocks } from '../../../tests/mocks/card/mocks';
+import { mocks, mockData } from '../../../tests/mocks/card/mocks';
 
-import { GET_CARD_DATA } from '~/src/graphql/queries/cardQuery';
+// import { GET_CARD_DATA } from '~/src/graphql/queries/cardQuery';
 
-const schema = makeExecutableSchema({ typeDefs });
-const preserveResolvers = false;
-const server = mockServer(schema, mocks, preserveResolvers);
+type SummonerArgs = {
+  name?: string;
+};
 
-export async function POST() {
-  const query =
-    typeof GET_CARD_DATA === 'string' ? GET_CARD_DATA : print(GET_CARD_DATA);
-  const variables = {};
-  // console.log('API Request');
-  try {
-    const response = await server.query(query, variables);
-    // console.log(response);
+let globalData = [...mockData];
 
-    return NextResponse.json(response);
-  } catch (error) {
-    console.error(error);
+const searchedData = {
+  id: 4,
+  information: {
+    summonerName: '말비나33',
+    summonerLevel: 33,
+    summonerIcon: '4122'
+  },
+  season: ['2020', '2021', '2022'],
+  tendency: ['갱킹선호', '초중반지향', '캐리형'],
+  lanes: ['Mid', 'Top'],
+  champions: [
+    { name: '야스오', winRate: 55.5, gamesPlayed: 120, KDA: 3.2 },
+    { name: '제드', winRate: 60.0, gamesPlayed: 100, KDA: 4.1 },
+    { name: '아트록스', winRate: 50.0, gamesPlayed: 90, KDA: 2.8 }
+  ]
+};
+
+const resolvers = {
+  Query: {
+    summoner: (_: any, args: SummonerArgs) => {
+      if (
+        args.name === '말비나33' &&
+        !globalData.some(data => data.id === searchedData.id)
+      ) {
+        globalData = [searchedData, ...globalData];
+      }
+      return globalData;
+    }
   }
-}
+};
+
+const server = new ApolloServer({
+  schema: addMocksToSchema({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    mocks,
+    preserveResolvers: true
+  })
+});
+
+// export async function POST(req: Request) {
+//   const data = await req.json();
+//   console.log(data.variables.name);
+
+//   const query =
+//     typeof GET_CARD_DATA === 'string' ? GET_CARD_DATA : print(GET_CARD_DATA);
+
+//   try {
+//     const response = await server.executeOperation({
+//       query: query,
+//       variables: { name: data.variables.name }
+//     });
+//     console.log(response);
+
+//     return NextResponse.json(response);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+const handler = startServerAndCreateNextHandler(server);
+
+export { handler as GET, handler as POST };
