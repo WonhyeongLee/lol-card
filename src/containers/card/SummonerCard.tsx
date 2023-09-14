6bdceb40809c41dc5b1ad30c6c56bd4ccdfa290a
WonhyeongLee <bigfile57@naver.com>
2023-09-14 22:21:38 +0900

    fix: SummonerCard가 인자를 받지 않아도 오류가 나지 않게 수정, use client 추가
    - SummonerCard를 SSR 컴포넌트에서 사용하면 use client를 붙여서 SummonerCard는 CSR로 동작하도록 해줘야해서 수정했습니다.

--- src/containers/card/SummonerCard.tsx
+++ src/containers/card/SummonerCard.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useRef } from 'react';
 
 import useCardAnimation from '~/src/hooks/useCardAnimation';
@@ -22,7 +23,7 @@ type SummonerProps = {
   champions: Champion[];
 };
 
-const SummonerCard: React.FC<{ summoner: SummonerProps }> = ({ summoner }) => {
+const SummonerCard: React.FC<{ summoner?: SummonerProps }> = ({ summoner }) => {
   const cardRef = useRef(null);
   useCardAnimation(cardRef);
 
@@ -32,22 +33,22 @@ const SummonerCard: React.FC<{ summoner: SummonerProps }> = ({ summoner }) => {
       <div className="mb-4 flex w-full items-center justify-between max-[440px]:mb-2">
         <div className="flex flex-shrink-0 items-center md:mb-0">
           <img
-            src={`https://ddragon.leagueoflegends.com/cdn/13.16.1/img/profileicon/${summoner.information.summonerIcon}.png`}
-            alt={`${summoner.information.summonerName}'s icon`}
+            src={`https://ddragon.leagueoflegends.com/cdn/13.16.1/img/profileicon/${summoner?.information.summonerIcon}.png`}
+            alt={`${summoner?.information.summonerName}'s icon`}
             className="mr-4 h-20 w-20 max-[440px]:h-12 max-[440px]:w-12"
           />
           <div className="mt-3 flex flex-col items-start max-[440px]:mt-0">
             <span className="rounded-full bg-blue-500 px-3 py-1 text-sm text-white max-[440px]:px-2 max-[440px]:text-xs">
-              {summoner.information.summonerLevel}
+              {summoner?.information.summonerLevel}
             </span>
             <span className="mt-1 text-2xl font-bold max-[440px]:text-base">
-              {summoner.information.summonerName}
+              {summoner?.information.summonerName}
             </span>
           </div>
         </div>
         {/* 라인 section */}
         <div className="max-w-[440px]:space-x-1 flex space-x-2">
-          {summoner.lanes.map((lane, index) => (
+          {summoner?.lanes.map((lane, index) => (
             <div
               key={index}
               className="h-20 w-20 rounded max-[440px]:h-12 max-[440px]:w-12"
@@ -64,7 +65,7 @@ const SummonerCard: React.FC<{ summoner: SummonerProps }> = ({ summoner }) => {
 
       {/* Section 2: 성향 */}
       <div className="mb-4 flex w-full flex-wrap gap-2 max-[440px]:mb-0">
-        {summoner.tendency.map((tend, index) => (
+        {summoner?.tendency.map((tend, index) => (
           <span key={index} className="rounded bg-gray-200 px-2 py-1">
             {tend}
           </span>
@@ -76,7 +77,7 @@ const SummonerCard: React.FC<{ summoner: SummonerProps }> = ({ summoner }) => {
         ref={cardRef}
         className="relative mx-auto mb-4 h-full w-2/3 max-[440px]:mb-0 max-[440px]:h-[90%] max-[440px]:w-full"
       >
-        {summoner.champions.map(champion => (
+        {summoner?.champions.map(champion => (
           <li
             key={champion.name}
             className={`absolute mt-[-0.5rem] flex h-full w-full transform flex-col rounded-md border p-2 max-[440px]:h-[95%]`}
