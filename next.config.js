7c6ceda9a24dabbe9dc2cf2d6db2fdc0cd81b252
WonhyeongLee <bigfile57@naver.com>
2023-09-18 19:04:45 +0900

    fix: next.config.js 설정에서 image의 remotePatterns 를 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/qBbP7bc/**'
+      }
+    ]
+  }
+};
 
 module.exports = nextConfig;
