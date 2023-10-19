71dfd896d441a9bd97da82e74b062e59fa47dce4
WonhyeongLee <bigfile57@naver.com>
2023-10-19 15:30:25 +0900

    fix: useCardData 훅의 타입을 제네릭을 추가
    - 각 컴포넌트들이 매개변수를 받지 않고 useCardData 훅을 사용하도록 리팩터링되는 과정에서 타입 에러를 해결하기 위해 제네릭타입을 이용해 수정했습니다.

--- src/hooks/useCardData.ts
+++ src/hooks/useCardData.ts
@@ -1,17 +1,21 @@
 'use client';
 import { useParams } from 'next/navigation';
 
+import { Summoner } from '../types/types';
+
 import { useAppSelector } from '~/src/redux/hooks';
 
-export const useCardData = (filterKey: string) => {
+export const useCardData = <T>(filterKey: keyof Summoner): T | null => {
   const params = useParams();
   const uuid = params.slug as string;
 
-  const cardData = useAppSelector(
+  const cardData: Summoner[] | undefined = useAppSelector(
     state => state.summonerReducer.cardData[uuid]
   );
 
-  const filteredData = cardData ? cardData[0][filterKey] : null;
+  const filteredData: T | null = cardData
+    ? (cardData[0][filterKey] as T)
+    : null;
 
   return filteredData;
 };
