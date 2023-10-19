028f6f55a01e18084a560897b5fe075fd813a975
WonhyeongLee <bigfile57@naver.com>
2023-10-20 01:02:12 +0900

    refact: CustomChampionList의 챔피언 선택부분 분리해서 ChampionSelectList 컴포넌트로 생성

--- src/containers/card/CustomizeChampionList.tsx
+++ src/containers/card/CustomizeChampionList.tsx
@@ -1,28 +1,13 @@
 import React from 'react';
 
 import ChampionCardList from './ChampionCardList';
+import ChampionSelectList from '../customize/ChampionSelectList';
 
 const CustomizeChampionList = () => {
   return (
     <>
-      <div className="flex h-1/5 sm:h-full sm:w-1/3">
-        <div className="mx-auto box-content flex justify-items-center gap-3 overflow-x-auto sm:grid sm:grid-cols-3 sm:grid-rows-5 sm:items-center sm:justify-items-center sm:overflow-visible">
-          <div className="h-16 w-16 overflow-hidden border-solid border-black">
-            <img className="h-full w-full object-cover object-center" />
-          </div>
-          <div className="h-16 w-16 border-solid border-black">
-            <img className="h-full w-full object-cover object-center" />
-          </div>
-          <div className="h-16 w-16 border-solid border-black">
-            <img className="h-full w-full object-cover object-center" />
-          </div>
-          <div className="h-16 w-16 border-solid border-black">
-            <img className="h-full w-full object-cover object-center" />
-          </div>
-          <div className="h-16 w-16 border-solid border-black">
-            <img className="h-full w-full object-cover object-center" />
-          </div>
-        </div>
+      <div className="flex h-1/5 items-center justify-center sm:h-full sm:w-1/3">
+        <ChampionSelectList />
       </div>
       <div className="mt-4 flex h-4/5 w-full items-center justify-center px-4 sm:h-full sm:w-2/3">
         <ChampionCardList />
---
+++ src/containers/customize/ChampionSelectList.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+const ChampionSelectList = () => {
+  return (
+    <div className="mx-auto box-content flex justify-items-center gap-3 overflow-x-auto sm:grid sm:grid-cols-3 sm:grid-rows-5 sm:items-center sm:justify-items-center sm:overflow-visible">
+      <div className="h-20 w-20 overflow-hidden border-solid border-black ">
+        <img className="h-full w-full object-cover object-center" />
+      </div>
+      <div className="h-20 w-20 border-solid border-black">
+        <img className="h-full w-full object-cover object-center" />
+      </div>
+      <div className="h-20 w-20 border-solid border-black">
+        <img className="h-full w-full object-cover object-center" />
+      </div>
+      <div className="h-20 w-20 border-solid border-black">
+        <img className="h-full w-full object-cover object-center" />
+      </div>
+      <div className="h-20 w-20 border-solid border-black">
+        <img className="h-full w-full object-cover object-center" />
+      </div>
+    </div>
+  );
+};
+
+export default ChampionSelectList;
