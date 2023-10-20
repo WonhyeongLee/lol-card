c02c5a2161d769b8f2031aade01ce51965f70401
WonhyeongLee <bigfile57@naver.com>
2023-10-20 21:50:03 +0900

    fix: SummonerCard가 cardData 저장 전에 렌더링 되는 문제 임시방편으로 처리

--- src/app/card/[slug]/(card)/page.tsx
+++ src/app/card/[slug]/(card)/page.tsx
@@ -1,4 +1,6 @@
 'use client';
+import { useState } from 'react';
+
 import { useQuery } from '@apollo/client';
 
 import SummonerCard from '~/src/containers/card/SummonerCard';
@@ -14,6 +16,7 @@ export default function Card({ params }: { params: { slug: string } }) {
     state => state.summonerReducer.summonerNames[uuid]
   );
   const dispatch = useAppDispatch();
+  const [isDataReady, setIsDataReady] = useState(false);
 
   const { loading, error, data } = useQuery<{ summoner: Summoner[] }>(
     GET_CARD_DATA,
@@ -22,6 +25,7 @@ export default function Card({ params }: { params: { slug: string } }) {
       skip: !summonerName,
       onCompleted: data => {
         dispatch(setCardData({ uuid, data: data.summoner }));
+        setIsDataReady(true);
       }
     }
   );
@@ -30,17 +34,18 @@ export default function Card({ params }: { params: { slug: string } }) {
     return <div>소환사 이름이 없습니다.</div>;
   }
 
-  if (loading) {
+  if (loading || !isDataReady) {
     return <div>Loading...</div>;
   }
   if (error) {
     return <div>Error: {error.message}</div>;
   }
+  console.log(data);
 
   return (
     <>
       <section className="relative flex h-auto min-h-[70vh] w-full max-w-[600px] flex-col items-center rounded-md bg-slate-100 p-4 shadow-lg">
-        {data?.summoner.map(summoner => <SummonerCard key={summoner.id} />)}
+        <SummonerCard />
         <CardNav />
       </section>
     </>
--- src/containers/card/ChampionCardList.tsx
+++ src/containers/card/ChampionCardList.tsx
@@ -12,25 +12,49 @@ const ChampionCardList = () => {
     useCardData<Champion[]>('champions');
   const cardRef = useRef(null);
   useCardAnimation(cardRef);
+
+  console.log('ChampionListData:', ChampionListData);
+
+  const displayedChampions = ChampionListData
+    ? ChampionListData.slice(0, 3)
+    : Array(3).fill(null);
+  console.log('displayedChampions:', displayedChampions);
+
+  const handleClick = (e: React.MouseEvent) => {
+    console.log('Element clicked:', e.target);
+  };
+
   return (
     <ul
       ref={cardRef}
       className="relative mx-auto mb-4 h-[60vh] min-h-[60vh] w-2/3 max-w-[365px] max-[440px]:my-2 max-[440px]:mb-0 max-[440px]:h-[90%] max-[440px]:w-full"
     >
-      {(ChampionListData || Array(3).fill(null)).map((champion, index) => (
+      {displayedChampions?.map((champion, index) => (
         <li
           key={champion ? champion.name : index}
           className={`absolute mt-[-0.5rem] flex w-full transform flex-col rounded-md border p-1`}
+          onClick={handleClick}
         >
           <div className="relative h-auto rounded-md bg-gray-200 p-2 text-center">
             {champion ? (
               <div className="relative h-[499px] overflow-hidden">
+                {/* <img
+                  src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.name}_0.jpg`}
+                  alt={champion.name}
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'contain'
+                  }}
+                /> */}
                 <Image
                   src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.name}_0.jpg`}
                   alt={champion.name}
+                  // width={339}
+                  // height={499}
                   fill={true}
-                  priority={true}
-                  style={{ objectFit: 'cover' }}
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  placeholder="empty"
                 />
                 <div className="absolute bottom-4 left-0 right-0 z-10 flex items-end justify-center pb-2 text-xl font-black text-white">
                   Win Rate: {champion.winRate}%<br /> Games Played:{' '}
