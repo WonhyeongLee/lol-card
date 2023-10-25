e458f0d36b9c704975ddd2a032cdbdf2e0efc6fb
WonhyeongLee <bigfile57@naver.com>
2023-10-25 23:34:46 +0900

    refactor: Card컴포넌트는 SSR로 SummonerCard는 CSR로 리팩토링
    - Card 페이지 컴포넌트에서 SummonerCard로 data 로직 옮기고 useSuspenseQuery를 사용함으로 Suspense로 로딩, 스켈레톤 컴포넌트를 사용할 수 있게 구현했습니다.

--- src/app/card/[slug]/(card)/page.tsx
+++ src/app/card/[slug]/(card)/page.tsx
@@ -1,51 +1,17 @@
-'use client';
-import { useState } from 'react';
+import { Suspense } from 'react';
 
-import { useQuery } from '@apollo/client';
+import Loading from './Loading';
 
 import SummonerCard from '~/src/containers/card/SummonerCard';
 import CardNav from '~/src/containers/nav/CardNav';
-import { GET_CARD_DATA } from '~/src/graphql/queries/cardQuery';
-import { setCardData } from '~/src/redux/features/summonerSlice';
-import { useAppSelector, useAppDispatch } from '~/src/redux/hooks';
-import { Summoner } from '~/src/types/types';
 
-export default function Card({ params }: { params: { slug: string } }) {
-  const uuid = params.slug;
-  const summonerName = useAppSelector(
-    state => state.summonerReducer.summonerNames[uuid]
-  );
-  const dispatch = useAppDispatch();
-  const [isDataReady, setIsDataReady] = useState(false);
-
-  const { loading, error, data } = useQuery<{ summoner: Summoner[] }>(
-    GET_CARD_DATA,
-    {
-      variables: { name: summonerName },
-      skip: !summonerName,
-      onCompleted: data => {
-        dispatch(setCardData({ uuid, data: data.summoner }));
-        setIsDataReady(true);
-      }
-    }
-  );
-
-  if (!summonerName) {
-    return <div>소환사 이름이 없습니다.</div>;
-  }
-
-  if (loading || !isDataReady) {
-    return <div>Loading...</div>;
-  }
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-  console.log(data);
-
+export default function Card() {
   return (
     <>
       <section className="relative flex h-auto min-h-[70vh] w-full max-w-[600px] flex-col items-center rounded-md bg-slate-100 p-4 shadow-lg">
-        <SummonerCard />
+        <Suspense fallback={<Loading />}>
+          <SummonerCard />
+        </Suspense>
         <CardNav />
       </section>
     </>
--- src/containers/card/SummonerCard.tsx
+++ src/containers/card/SummonerCard.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+'use client';
+import { useSuspenseQuery } from '@apollo/client';
+import { usePathname } from 'next/navigation';
 
 import ChampionCardList from './ChampionCardList';
 import SummonerInfo from './SummonerInfo';
 import SummonerTendency from './SummonerTendency';
 
+import { GET_CARD_DATA } from '~/src/graphql/queries/cardQuery';
+import { setCardData } from '~/src/redux/features/summonerSlice';
+import { useAppSelector, useAppDispatch } from '~/src/redux/hooks';
+import { Summoner } from '~/src/types/types';
+
 const SummonerCard = () => {
+  const pathname = usePathname();
+  const uuid = pathname.split('/').pop() as string;
+
+  const summonerName = useAppSelector(
+    state => state.summonerReducer.summonerNames[uuid]
+  );
+  const dispatch = useAppDispatch();
+
+  const { data } = useSuspenseQuery<{ summoner: Summoner[] }>(GET_CARD_DATA, {
+    variables: { name: summonerName }
+  });
+
+  if (data) {
+    dispatch(setCardData({ uuid, data: data.summoner }));
+  }
+
   return (
     <>
       <SummonerInfo />
