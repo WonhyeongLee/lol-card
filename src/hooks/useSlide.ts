8faef246737b360358009db2b43fe5d278e5e35c
WonhyeongLee <bigfile57@naver.com>
2023-10-30 23:55:47 +0900

    feat: useSlide 훅을 상하 슬라이드 말고 좌우 슬라이드도 가능하도록 기능추가

--- src/hooks/useSlide.ts
+++ src/hooks/useSlide.ts
@@ -3,18 +3,38 @@ import { useRef, useState } from 'react';
 import gsap from 'gsap';
 
 type UseSlideProps = {
-  sectionHeight: number;
-  numSections: number;
+  sectionHeight?: number;
+  sectionWidth?: number;
+  numSections?: number;
+  numHorizontalSections?: number;
 };
 
-export const useSlide = ({ sectionHeight, numSections }: UseSlideProps) => {
+export const useSlide = ({
+  sectionHeight = 0,
+  sectionWidth = 0,
+  numSections = 0,
+  numHorizontalSections = 0
+}: UseSlideProps) => {
   const containerRef = useRef(null);
   const [currentSection, setCurrentSection] = useState(0);
+  const [currentHorizontalSection, setCurrentHorizontalSection] = useState(0);
 
-  const moveToSection = (sectionIndex: number) => {
-    const yValue = -sectionHeight * sectionIndex;
-    gsap.to(containerRef.current, { y: `${yValue}px`, duration: 1 });
-    setCurrentSection(sectionIndex);
+  const moveToSection = (
+    sectionIndex: number,
+    isHorizontal: boolean = false
+  ) => {
+    if (isHorizontal) {
+      const xValue = -sectionWidth * sectionIndex;
+      gsap.to(containerRef.current, {
+        x: `${xValue}px`,
+        duration: 1
+      });
+      setCurrentHorizontalSection(sectionIndex);
+    } else {
+      const yValue = -sectionHeight * sectionIndex;
+      gsap.to(containerRef.current, { y: `${yValue}px`, duration: 1 });
+      setCurrentSection(sectionIndex);
+    }
   };
 
   const slideUp = () => {
@@ -29,9 +49,23 @@ export const useSlide = ({ sectionHeight, numSections }: UseSlideProps) => {
     }
   };
 
+  const slideLeft = () => {
+    if (currentHorizontalSection > 0) {
+      moveToSection(currentHorizontalSection - 1, true);
+    }
+  };
+
+  const slideRight = () => {
+    if (currentHorizontalSection < numHorizontalSections - 1) {
+      moveToSection(currentHorizontalSection + 1, true);
+    }
+  };
+
   return {
     containerRef,
     slideUp,
-    slideDown
+    slideDown,
+    slideLeft,
+    slideRight
   };
 };
