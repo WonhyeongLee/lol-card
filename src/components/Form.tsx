9aaa213af0d1c70f3565ee2bb383435e62cb4d9e
WonhyeongLee <bigfile57@naver.com>
2023-11-08 20:38:46 +0900

    fix: Input, Form 스타일 변경, label 추가 및 버튼 아이콘 추가

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,6 +5,7 @@ import { InputComponent } from '~components/Input';
 
 type FormField = {
   name: string;
+  label: string;
   value: string;
   type: string;
   placeholder: string;
@@ -18,12 +19,11 @@ type FormProps = {
 export default function Form({ fields, onSubmit }: FormProps) {
   const [formValues, setFormValues] = useState<Record<string, string>>({});
 
-  //상황에 따라 debounce 적용 고려
   const handleChange = (name: string, value: string) => {
-    setFormValues({
-      ...formValues,
+    setFormValues(prevValues => ({
+      ...prevValues,
       [name]: value
-    });
+    }));
   };
 
   const handleSubmit = (e: FormEvent) => {
@@ -32,19 +32,33 @@ export default function Form({ fields, onSubmit }: FormProps) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="relative flex w-1/2 flex-col">
-      {fields.map(field => (
-        <InputComponent
-          key={field.name}
-          name={field.name}
-          type={field.type}
-          placeholderText={field.placeholder}
-          onInputChange={value => handleChange(field.name, value)}
-        />
-      ))}
-      <div>
-        <button type="submit">Submit</button>
+    <form onSubmit={handleSubmit}>
+      <div className="relative h-full">
+        {fields.map(field => (
+          <InputComponent
+            key={field.name}
+            label={field.label}
+            name={field.name}
+            type={field.type}
+            placeholderText={field.placeholder}
+            onInputChange={value => handleChange(field.name, value)}
+          />
+        ))}
       </div>
+      <button type="submit" className="absolute inset-y-0 bottom-1 right-0 p-2">
+        <svg
+          className="h-5 w-5 text-gray-700"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path d="M15.5 14L19 17.5"></path>
+          <circle cx="10" cy="10" r="6"></circle>
+        </svg>
+      </button>
     </form>
   );
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,6 +2,7 @@
 import { useState, ChangeEvent } from 'react';
 
 type InputComponentProps = {
+  label: string;
   name: string;
   type: string;
   placeholderText: string;
@@ -9,6 +10,7 @@ type InputComponentProps = {
 };
 export const InputComponent = ({
   name,
+  label,
   type,
   placeholderText,
   onInputChange
@@ -21,13 +23,22 @@ export const InputComponent = ({
   };
 
   return (
-    <input
-      name={name}
-      type={type}
-      className="m-auto mb-4 w-full min-w-[200px] rounded-lg border p-2 text-center placeholder-opacity-0 focus:placeholder-opacity-0"
-      placeholder={placeholderText}
-      value={inputValue}
-      onChange={handleInputChange}
-    />
+    <div className="relative m-auto w-full min-w-[200px] rounded-lg border bg-white p-2 text-center placeholder-opacity-0 focus-within:border-blue-500 focus:placeholder-opacity-0">
+      <label
+        htmlFor={name}
+        className="absolute left-4 top-1 px-2 text-xs font-medium text-gray-500"
+      >
+        {label}
+      </label>
+      <input
+        className="h-full w-full px-3 py-2 text-base placeholder-gray-500 focus:outline-none"
+        id={name}
+        name={name}
+        type={type}
+        placeholder={placeholderText}
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+    </div>
   );
 };
