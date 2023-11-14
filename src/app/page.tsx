adfb143208b9a8a0a6fe06b008a36ea22ce906c7
WonhyeongLee <bigfile57@naver.com>
2023-11-14 20:15:58 +0900

    fix: card페이지에서 main페이지 이동할 때 변한 쿠키내용 반영되도록 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,24 +18,31 @@ const GET_CARD_DATA = gql`
   ${CardTitleFragment}
 `;
 
-export const dynamic = 'force-dynamic';
-
-export default async function Home() {
+async function fetchRecentCardData() {
   const summonerName = getSummonerNameValue('summonerName');
-  const { data } = await getClient().query({
+  const res = await getClient().query({
     query: GET_CARD_DATA,
     variables: { names: summonerName }
   });
+
+  return res;
+}
+export default async function Home() {
+  const { data } = await fetchRecentCardData();
 
   return (
     <main className="flex min-h-screen justify-center">
       <section className="mx-20 flex w-full max-w-screen-lg flex-col items-center justify-center p-8">
         <div className="mb-4 text-center text-4xl font-extrabold">LOL-CARD</div>
-        <div className="relative mb-4 flex w-1/2 flex-col">
+        <div className="relative mb-4 flex w-1/2 min-w-[324px] flex-col">
           <SummonerSearchFormContainer />
         </div>
-        <span>최근 작성한 롤카드</span>
-        <RecentCard recentCardData={data.summoner} />
+        {data && data.summoner && data.summoner.length > 0 ? (
+          <>
+            <span>최근 작성한 롤카드</span>
+            <RecentCard recentCardData={data.summoner} />
+          </>
+        ) : null}
       </section>
     </main>
   );
--- src/containers/nav/CardNav.tsx
+++ src/containers/nav/CardNav.tsx
@@ -1,21 +1,25 @@
 'use client';
 import React from 'react';
 
-import { useRouter } from 'next/navigation';
-import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import { useRouter, usePathname } from 'next/navigation';
 
 const CardNav = () => {
   const router = useRouter();
   const pathname = usePathname();
 
+  const handleRefresh = () => {
+    router.refresh();
+  };
+
   return (
-    <article className="absolute -bottom-5 flex h-12 flex-row gap-2 rounded-md rounded-b-md bg-red-100 px-1 py-1 shadow-lg md:left-[-6rem] md:top-4 md:h-auto md:min-h-fit md:flex-col md:py-4 md:transition-all md:duration-300 md:ease-in-out">
-      <button onClick={() => router.push('/')}> 뒤로가기</button>
-      <button onClick={() => router.push(`${pathname}/custom`)}>
-        {' '}
-        커스텀하기
-      </button>
-      <button onClick={() => router.push('/')}> 뒤로가기</button>
+    <article className="absolute bottom-16 z-20 flex-row gap-4 rounded-md rounded-b-md bg-red-100 px-2 py-2 shadow-lg ">
+      <div className="flex justify-center gap-4">
+        <Link href="/" onClick={handleRefresh}>
+          홈으로
+        </Link>
+        <Link href={`${pathname}/custom`}>커스텀</Link>
+      </div>
     </article>
   );
 };
